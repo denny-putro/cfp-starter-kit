840d08e797654e9fe2c3bb00d4ad02bf5a1f85a8
denny-putro <denny.putro@traveloka.com>
2022-09-29 10:52:49 +0700

    add hotjar

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,14 @@ module.exports = {
     title: `cfp-starter-kit`,
     siteUrl: `https://www.yourdomain.tld`
   },
-  plugins: ["gatsby-plugin-sass"]
+  plugins: ["gatsby-plugin-sass",
+    {
+      resolve: `gatsby-plugin-hotjar`,
+      options: {
+        includeInDevelopment: true, // optional parameter to include script in development
+        id: 3178592,
+        sv: 6,
+      },
+    },
+  ]
 };
